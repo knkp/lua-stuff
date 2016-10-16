b4c74bd1cd3010ad7167d7a75a630d051565796f
knkp <knkp934@gmail.com>
2016-10-16 14:38:44 -0400

    initial commit

---
+++ hello.lua
@@ -0,0 +1,17 @@
+-- testing input stuff
+
+local inputStr = string.format("hello")
+
+inputStr = io.read()
+
+print(inputStr)
+
+
+---- flow control ----
+
+
+if string.find(inputStr, "hello") then
+	print("found it")
+else
+	print("didn't find it")
+end
